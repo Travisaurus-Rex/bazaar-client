ae74b55b4d01b7f511633bd8d24beda01d3da378
Travis Adams <brokenCD@live.com>
2025-11-22 19:39:08 +0700

    create header nav component

---
+++ src/app/shared/components/nav-component/nav-header.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-nav-header',
+  imports: [RouterLink],
+  templateUrl: './nav-header.html',
+  styleUrl: './nav-header.scss',
+})
+export class NavHeaderComponent {
+
+}
