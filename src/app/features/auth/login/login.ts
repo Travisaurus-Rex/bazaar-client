9130253af423d78526a226b8eec75558e1667feb
Travis Adams <brokenCD@live.com>
2025-11-24 12:51:35 +0700

    add tailwind and improve login and registration forms

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,32 +1,37 @@
 import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from 'app/shared/services/auth-service';
-import { Router, RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, RouterLink],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrls: ['./login.scss'],
 })
 export class LoginComponent {
-  auth = inject(AuthService);
-  router = inject(Router);
+  auth: AuthService = inject(AuthService);
+  router: Router = inject(Router);
 
   email = '';
   password = '';
   error = signal<string | null>(null);
 
-  onSubmit() {
-    this.auth.login(this.email, this.password).subscribe({
-      next: () => {
-        this.router.navigate(['/products']);
-      },
-      error: (err) => {
-        this.error.set(err.error?.message || 'Invalid login');
-      }
-    });
+  async onSubmit(): Promise<void> {
+    this.error.set(null);
+
+    try {
+      await firstValueFrom(
+        this.auth.login(this.email, this.password)
+      );
+
+      this.router.navigate(['/products']);
+
+    } catch (err: any) {
+      this.error.set(err.error?.message || 'Invalid login');
+    }
   }
 }
 
--- src/app/features/auth/register/register.ts
+++ src/app/features/auth/register/register.ts
@@ -1,33 +1,37 @@
 import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from 'app/shared/services/auth-service';
-import { Router, RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule, RouterLink],
+  imports: [FormsModule],
   templateUrl: './register.html',
   styleUrls: ['./register.scss'],
 })
 export class RegisterComponent {
-  auth = inject(AuthService);
-  router = inject(Router);
+  auth: AuthService = inject(AuthService);
+  router: Router = inject(Router);
 
   email = '';
   password = '';
   role: 'buyer' | 'seller' = 'buyer';
   error = signal<string | null>(null);
 
-  onSubmit() {
-    this.auth.register(this.email, this.password, this.role).subscribe({
-      next: () => {
-        this.router.navigate(['/products']);
-      },
-      error: (err) => {
-        this.error.set(err.error?.message || 'Registration failed');
-      }
-    });
+  async onSubmit(): Promise<void> {
+    this.error.set(null);
+
+    try {
+      await lastValueFrom(
+        this.auth.register(this.email, this.password, this.role)
+      )
+
+      this.router.navigate(['/products']);
+    } catch {
+      this.error.set('Registration failed')
+    }
   }
 }
 
